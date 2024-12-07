6a59862682c7de05422aae5f9f139f5b6c7e6c31
Emanuel Hilacondo <ehilacondob@unsa.edu.pe>
2024-12-07 22:41:51 +0000

    Se agregó la lógica del javascript, habrnán cuadro cajas en cada una el ususario ingresará el main, la herencia, el interface, las clases composicion, agregacion y demás

--- UML_BackEnd/echo.pl
+++ UML_BackEnd/echo.pl
@@ -17,24 +17,81 @@ Content-type: text/html
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        .panel-title {
+            cursor: pointer;
+        }
+        .panel-body {
+            display: none;
+        }
+    </style>
 </head>
 <body>
 <div class="container">
     <h1 class="text-center">Generador de Diagramas UML</h1>
     <hr>
     <form id="umlForm" class="form-horizontal" action="generate_uml.pl" method="post">
-        <div class="form-group">
-            <label for="javaCode" class="col-sm-2 control-label">Código Java:</label>
-            <div class="col-sm-10">
-                <textarea id="javaCode" name="java_code" class="form-control" rows="10" placeholder="Escribe tu código Java aquí..."></textarea>
+        <!-- Sección de Main -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title" data-toggle="collapse" data-target="#mainBox">Main</h4>
+            </div>
+            <div id="mainBox" class="panel-body">
+                <textarea id="javaCodeMain" name="java_code_main" class="form-control" rows="5" placeholder="Escribe el código de la clase Main aquí..."></textarea>
             </div>
         </div>
+
+        <!-- Sección de Herencia -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title" data-toggle="collapse" data-target="#inheritanceBox">Herencia</h4>
+            </div>
+            <div id="inheritanceBox" class="panel-body">
+                <textarea id="javaCodeInheritance" name="java_code_inheritance" class="form-control" rows="5" placeholder="Escribe el código de la clase Herencia aquí..."></textarea>
+            </div>
+        </div>
+
+        <!-- Sección de Interface -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title" data-toggle="collapse" data-target="#interfaceBox">Interface</h4>
+            </div>
+            <div id="interfaceBox" class="panel-body">
+                <textarea id="javaCodeInterface" name="java_code_interface" class="form-control" rows="5" placeholder="Escribe el código de la Interface aquí..."></textarea>
+            </div>
+        </div>
+
+        <!-- Sección de Clase -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title" data-toggle="collapse" data-target="#classBox">Clase</h4>
+            </div>
+            <div id="classBox" class="panel-body">
+                <textarea id="javaCodeClass" name="java_code_class" class="form-control" rows="5" placeholder="Escribe el código de la Clase aquí..."></textarea>
+            </div>
+        </div>
+
         <div class="form-group text-center">
             <button type="submit" class="btn btn-primary">Generar UML</button>
         </div>
     </form>
     <hr>
 </div>
+
+<script>
+$(document).ready(function() {
+    // Aseguramos que las cajas de texto estén ocultas al principio
+    $('.panel-body').hide();
+
+    // Manejamos el clic en los títulos para mostrar/ocultar las cajas de texto
+    $('.panel-title').on('click', function() {
+        var target = $(this).data('target');
+        $(target).toggle();  // Cambiar visibilidad del panel correspondiente
+    });
+});
+</script>
 </body>
 </html>
 EOF
+
+
