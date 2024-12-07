2444cecd0788232ebeab2658ab98ed03cedce605
Emanuel Hilacondo <ehilacondob@unsa.edu.pe>
2024-12-07 22:45:34 +0000

    Se agregó los cgi para recibir lso parametros del echo.pl, y el inicio de la lógica para leer y crear el archivo puml

--- UML_BackEnd/generate_uml.pl
+++ UML_BackEnd/generate_uml.pl
@@ -8,24 +8,22 @@ use File::Path qw(make_path);
 # Crear objeto CGI
 my $cgi = CGI->new;
 
-# Leer el parámetro enviado por POST
-my $java_code = $cgi->param('java_code');
+# Leer los parámetros enviados por POST
+my $java_code_main = $cgi->param('java_code_main');
+my $java_code_inheritance = $cgi->param('java_code_inheritance');
+my $java_code_interface = $cgi->param('java_code_interface');
+my $java_code_class = $cgi->param('java_code_class');
 
 # Imprimir encabezado HTTP y generar el HTML dinámico
 print $cgi->header(-type => 'text/html', -charset => 'UTF-8');
 
 # Función para convertir código Java a PlantUML
 sub java_to_puml {
-    my ($code) = @_;
-    my $uml = '@startuml' . "\n";
+    my ($code, $class_name) = @_;
+    my $uml = '';
 
-    # Extraer la clase principal
-    if ($code =~ /public\s+class\s+(\w+)/) {
-        my $class_name = $1;
-        $uml .= "class $class_name {\n";
-    } else {
-        return { error => "No se pudo identificar la clase principal en el código Java." };
-    }
+    # Si se proporciona un nombre de clase, se añade al archivo UML
+    $uml .= "class $class_name {\n" if $class_name;
 
     # Extraer métodos (ignorando comentarios y JavaDoc)
     while ($code =~ /public\s+(\w+)\s+(\w+)\s*\((.*?)\)/g) {
@@ -43,39 +41,36 @@ sub java_to_puml {
 
     # Cerrar definición de la clase
     $uml .= "}\n";
-    $uml .= '@enduml' . "\n";
-
-    return { uml => $uml };
+    return $uml;
 }
 
-# Generar contenido UML
-my $result = java_to_puml($java_code);
+# Generar el contenido UML para cada caja de texto (Main, Herencia, Interface, Clase)
+my $uml_content = '@startuml' . "\n";
 
-# Manejar errores en la conversión
-if ($result->{error}) {
-    print <<HTML;
-    <div class="alert alert-danger">
-        <p><strong>Error:</strong> $result->{error}</p>
-    </div>
-HTML
-    exit;
-}
+# Procesar cada sección de código
+$uml_content .= java_to_puml($java_code_main, 'Main') if $java_code_main;
+$uml_content .= java_to_puml($java_code_inheritance, 'Herencia') if $java_code_inheritance;
+$uml_content .= java_to_puml($java_code_interface, 'Interface') if $java_code_interface;
+$uml_content .= java_to_puml($java_code_class, 'Clase') if $java_code_class;
 
-# Guardar el archivo .puml
-my $filename = "output_uml_" . time() . ".puml";
+$uml_content .= '@enduml' . "\n";
+
+# Ruta para guardar el archivo .puml
+my $filename = "output_uml.puml";
 my $filepath = "E:/xampp/cgi-bin/prueba/$filename";
 
 # Asegurar que el directorio exista
 make_path('E:/xampp/cgi-bin/prueba') unless -d 'E:/xampp/cgi-bin/prueba';
 
+# Guardar el archivo .puml
 if (open(my $fh, '>', $filepath)) {
-    print $fh $result->{uml};
+    print $fh $uml_content;
     close($fh);
     print <<HTML;
     <div class="alert alert-success">
-        <p>¡Archivo UML generado exitosamente!</p>
+        <p>Archivo UML generado exitosamente</p>
         <p><strong>Contenido del archivo .puml:</strong></p>
-        <pre>$result->{uml}</pre>
+        <pre>$uml_content</pre>
         <p><a href="/prueba/$filename" download class="btn btn-success">Descargar UML</a></p>
     </div>
 HTML
